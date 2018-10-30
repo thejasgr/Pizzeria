15451f323ec285b3e9c2b440da2509d64f7ca6eb
A06438_P5.Training <A06438_P5.Training@accenture.com>
2018-10-30 15:47:41 +0530

    Angula App with coponent-Clean Slate

--- Pizzeria/src/app/app.module.ts
+++ Pizzeria/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeScreenComponent } from './home-screen/home-screen.component';
+import { PizzaOrderComponent } from './pizza-order/pizza-order.component';
+import { BuildUrPizzaComponent } from './build-ur-pizza/build-ur-pizza.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeScreenComponent,
+    PizzaOrderComponent,
+    BuildUrPizzaComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule
---
+++ Pizzeria/src/app/build-ur-pizza/build-ur-pizza.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-build-ur-pizza',
+  templateUrl: './build-ur-pizza.component.html',
+  styleUrls: ['./build-ur-pizza.component.css']
+})
+export class BuildUrPizzaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
